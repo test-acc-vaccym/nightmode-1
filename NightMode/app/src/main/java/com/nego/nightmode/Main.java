e8fdbcb42342b5850b5175bb31de266f4f164ac8
Tommaso Berlose <tommaso.berlose@gmail.com>
2015-10-14 18:50:41 +0200

    Modificate immagini main Mancano settings e about da sistemare, animazioni main, orari prefissati, widget, nfc

--- NightMode/app/src/main/java/com/nego/nightmode/Main.java
+++ NightMode/app/src/main/java/com/nego/nightmode/Main.java
@@ -21,6 +21,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,6 +35,7 @@ public class Main extends AppCompatActivity {
     private BroadcastReceiver mReceiver;
     private TextView ui_enabled;
     private TextView ui_start_time;
+    private Button button_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +45,33 @@ public class Main extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle("");
         button = (ImageView) findViewById(R.id.button_center);
+        button_title = (Button) findViewById(R.id.button_title);
         ui_enabled = (TextView) findViewById(R.id.app_isenabled);
         ui_start_time = (TextView) findViewById(R.id.start_time);
 
         SP = getSharedPreferences(Costants.PREFERENCES_COSTANT, Context.MODE_PRIVATE);
 
         updateUI(SP.getBoolean(Costants.PREFERENCES_NIGHT_MODE_ACTIVE, false));
-        button.setOnClickListener(new View.OnClickListener() {
+        button_title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switchNightModeWithText(!button.isSelected());
+                switchNightMode(!button.isSelected());
             }
         });
 
+        findViewById(R.id.action_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Main.this, Settings.class), 1);
+            }
+        });
+
+        findViewById(R.id.action_about).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Main.this, About.class), 1);
+            }
+        });
 
     }
 
@@ -87,40 +104,16 @@ public class Main extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            startActivityForResult(new Intent(this, Settings.class), 1);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void updateUI(boolean activated) {
-        button.animate().scaleX(0).scaleY(0).start();
         button.setSelected(activated);
-        button.setImageResource(activated ? R.drawable.ic_action_night_mode_on : R.drawable.ic_action_device_brightness_low);
-        button.animate().scaleX(1).scaleY(1).start();
-        ui_enabled.setText(activated ? R.string.app_name_enabled : R.string.app_name_disabled);
+        button_title.setText(activated ? R.string.action_disable : R.string.action_enable);
+        ui_enabled.setText(activated ? R.string.app_name_disabled : R.string.app_name_disabled);
         ui_start_time.setText(getString(activated ? R.string.start_time_enabled : R.string.start_time_disabled, Utils.getDate(this, SP.getLong(Costants.PREFERENCES_START_TIME, 0))));
     }
 
     public void switchNightMode(boolean activated) {
         updateUI(activated);
         NMToggle.startAction(this, activated ? Costants.ACTION_NIGHT_MODE_ON : Costants.ACTION_NIGHT_MODE_OFF);
-    }
-
-    public void switchNightModeWithText(boolean activated) {
-        switchNightMode(activated);
-        Snackbar.make(button, activated ? R.string.app_name_enabled : R.string.app_name_disabled , Snackbar.LENGTH_SHORT).show();
     }
 
 }
