c7bcbdcd13257a4af5de307241215ec883bd4815
Tommaso Berlose <tommaso.berlose@gmail.com>
2015-10-30 16:01:54 +0100

    Sistemato bootreceiver e aggiunta traduzione italiano

--- NightMode/app/src/main/java/com/nego/nightmode/Receiver/BootReceiver.java
+++ NightMode/app/src/main/java/com/nego/nightmode/Receiver/BootReceiver.java
@@ -4,9 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 
+import com.nego.nightmode.Alarm;
 import com.nego.nightmode.Costants;
+import com.nego.nightmode.Functions.AlarmF;
+import com.nego.nightmode.Functions.AlarmService;
 import com.nego.nightmode.Utils;
+import com.nego.nightmode.database.DbAdapter;
 
 public class BootReceiver extends BroadcastReceiver {
 
@@ -17,7 +22,16 @@ public class BootReceiver extends BroadcastReceiver {
             SharedPreferences SP = context.getSharedPreferences(Costants.PREFERENCES_COSTANT, Context.MODE_PRIVATE);
             Utils.showNotification(context, SP.getBoolean(Costants.PREFERENCES_NIGHT_MODE_ACTIVE, false));
 
-            // TODO reimpostare allarmi per attivazione/disattivazione
+
+            DbAdapter dbHelper = new DbAdapter(context);
+            dbHelper.open();
+            Cursor c = dbHelper.fetchAllAlarms();
+            while (c.moveToNext()) {
+                Alarm a = new Alarm(c);
+                AlarmF.addAlarm(context, a.getId() + 10000, a.getStart(), Costants.ACTION_NIGHT_MODE_ON);
+                AlarmF.addAlarm(context, a.getId(), a.getEnd(), Costants.ACTION_NIGHT_MODE_OFF);
+            }
+            dbHelper.close();
         }
     }
 }
